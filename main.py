9136050b414d1ba0f109b193ff501c2158a71eda
Bridger Fisher <laxfish@bridgers-mbp.devices.wcslc.net>
2017-12-05 17:32:36 -0700

    [Step 3] Added remove functionality

--- main.py
+++ main.py
@@ -22,23 +22,123 @@ def login():
 def register():
 	return render_template('user_systems/registrer.php')
 
-# @app.route('/search/')
-# def search():
-# 	db_cursor = db.cursor()
-# 	search_term = request.form['text']
-# 	# data = db_cursor.fetchone()
-# 	query = request.args("Select * from {}".format(search_term))
-# 	db_cursor.execute(query)
-# 	headers=[header[0] for header in db_cursor.description] #return headers with values
-# 	data = db_cursor.fetchall()
-# 	json_data=[]
-# 	for attribute in data:
-# 		json_data.append(dict(zip(headers,attribute)))
-# 	# return render_template('brewery_info.html', data=json.dumps(json_data, default=date_handler))
-# 	return render_template('search.html', search_term=search_term, search=(json_data))
-# @app.route('/search/')
-# def search():
-#     return render_template('search.html')
+@app.route('/advanced_search/')
+def advanced_search():
+	return render_template('advanced_search.html')
+
+@app.route('/admin/insert/')
+def admin_insert():
+	db_cursor = db.cursor()
+	query_list = []
+	breweries = "Select * from Brewery;"
+	query_list.append(breweries)
+	styles = "Select * from BEER_STYLE;"
+	query_list.append(styles)
+	availability = "Select * from SEASONAL_AVAILABILITY;"
+	query_list.append(availability)
+	price_range = "Select * from PRICE_RANGE;"
+	query_list.append(price_range)
+	regions = "Select * from Region;"
+	query_list.append(regions)
+
+	brewery_data = []
+	style_data = []
+	availability_data = []
+	price_range_data = []
+	regions_data = []
+
+	for query in range(0, len(query_list)):
+		db_cursor.execute(query_list[query])
+		headers=[header[0] for header in db_cursor.description] #return headers with values
+		data = db_cursor.fetchall()
+		for attribute in data:
+			if query == 0:
+				brewery_data.append(dict(zip(headers,attribute)))
+			elif query == 1:
+				style_data.append(dict(zip(headers,attribute)))
+			elif query == 2:
+				availability_data.append(dict(zip(headers,attribute)))
+			elif query == 3:
+				price_range_data.append(dict(zip(headers,attribute)))
+			else:
+				regions_data.append(dict(zip(headers,attribute)))
+
+	return render_template('insert.html', breweries=(brewery_data), styles=(style_data), prices=(price_range_data), availabilities=(availability_data), regions=(regions_data))
+
+@app.route('/admin/insert/brewery', methods=['GET', 'POST'])
+def insert_brewery():
+	db_cursor = db.cursor()
+	name = request.form["name"]
+	date_established = request.form["date_established"]
+	print "Date", date_established
+	phone_number = request.form["phone_number"]
+	website = request.form["website"]
+	city = request.form["city"]
+	state = request.form["state"]
+	country = request.form.get("country")
+	zipcode = int(request.form["zipcode"])
+	region_id = request.form.get("region_id")
+	history_description = request.form["history_description"]
+
+	insert_query = "INSERT INTO BREWERY (brewery_id, name, date_established, phone_number, website, city, country, state, zipcode, region_id, history_description) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
+	db_cursor.execute(insert_query, (17, str(name), str(date_established), str(phone_number), str(website), str(city), str(country), str(state), int(zipcode), int(region_id), str(history_description)))
+	db.commit()
+	# get_query = "Select * from Brewery;"
+    #
+	# db_cursor.execute(get_query)
+	# headers=[header[0] for header in db_cursor.description] #return headers with values
+	# data = db_cursor.fetchall()
+	# json_data=[]
+	# for attribute in data:
+	# 	json_data.append(dict(zip(headers,attribute)))
+
+	return render_template('insert.html')
+
+@app.route('/admin/insert/beer', methods=['GET', 'POST'])
+def insert_beer():
+	db_cursor = db.cursor()
+	name = request.form["name"]
+	date_established = request.form["date_established"]
+	print "Date", date_established
+	phone_number = request.form["phone_number"]
+	website = request.form["website"]
+	city = request.form["city"]
+	state = request.form["state"]
+	country = request.form.get("country")
+	zipcode = int(request.form["zipcode"])
+	region_id = request.form.get("region_id")
+	history_description = request.form["history_description"]
+
+	insert_query = "INSERT INTO BREWERY (brewery_id, name, date_established, phone_number, website, city, country, state, zipcode, region_id, history_description) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
+	db_cursor.execute(insert_query, (17, str(name), str(date_established), str(phone_number), str(website), str(city), str(country), str(state), int(zipcode), int(region_id), str(history_description)))
+	db.commit()
+	get_query = "Select * from Brewery;"
+
+	db_cursor.execute(get_query)
+	headers=[header[0] for header in db_cursor.description] #return headers with values
+	data = db_cursor.fetchall()
+	json_data=[]
+	for attribute in data:
+		json_data.append(dict(zip(headers,attribute)))
+
+	return render_template('breweries.html', data=(json_data))
+
+
+@app.route('/admin/login/')
+def admin_login():
+	username = request.form.get('username')
+	password = request.form.get('password')
+	db_cursor = db.cursor()
+	username_query = ("Select * from Person where username = '{}';".format(username))
+	password_query = ("Select * from Person where password = '{}';".format(password))
+	db_cursor.execute(query)
+	headers=[header[0] for header in db_cursor.description] #return headers with values
+	data = db_cursor.fetchall()
+	json_data=[]
+	for attribute in data:
+		json_data.append(dict(zip(headers,attribute)))
+	return render_template('admin_login.html', data=(json_data))
+
 
 @app.route('/search/', methods=['POST'])
 def search_post():
