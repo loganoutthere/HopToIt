dd6c3964de3169a69617ebfa57f00468fbab01d9
ThatsLeft <thomas2102@live.no>
2017-12-12 18:46:12 -0700

    procedure

---
+++ priceProcedure.sql
@@ -0,0 +1,10 @@
+#--to call CALL ChangePrice([Beer_id you want to change], [the new price_range_code])
+
+DROP PROCEDURE IF EXISTS ChangePrice;
+delimiter //
+CREATE PROCEDURE ChangePrice (IN BeerIN INT(10),IN NewRange INT(9) )
+BEGIN
+  UPDATE BEER set price_range_code = NewRange WHERE beer_id = BeerIN;
+END;
+//
+delimiter ;
